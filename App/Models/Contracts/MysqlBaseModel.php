1505ab5fc8b8c532114d885db1dbd855628a13a1
seymi <seyedali1377mir@gmail.com>
2022-01-04 20:12:40 +0330

    using package Medoo to implement MysqlBaseModel methods

--- App/Models/Contracts/MysqlBaseModel.php
+++ App/Models/Contracts/MysqlBaseModel.php
@@ -2,17 +2,48 @@
 
 namespace App\Models\Contracts;
 
+use Exception;
 use PDO;
-use PDOException;
+use Medoo\Medoo;
 
 class MysqlBaseModel extends BaseModel
 {
-    protected function __construct()
+    public function __construct()
     {
         try {
-            $this->connection = new PDO("mysql:dbname={$_ENV['DB_NAME']};host={$_ENV['DB_HOST']}", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-            $this->connection->exec('set names utf8;');
-        } catch (PDOException $e) {
+            $this->connection = new Medoo([
+                // [required]
+                'type' => 'mysql',
+                'host' => $_ENV['DB_HOST'],
+                'database' => $_ENV['DB_NAME'],
+                'username' => $_ENV['DB_USER'],
+                'password' => $_ENV['DB_PASSWORD'],
+             
+                // [optional]
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_general_ci',
+                'port' => 3306,
+             
+                // [optional] Table prefix, all table names will be prefixed as PREFIX_table.
+                'prefix' => '',
+             
+                // [optional] Enable logging, it is disabled by default for better performance.
+                'logging' => true,
+             
+                // [optional]
+                // Error mode
+                // Error handling strategies when error is occurred.
+                // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+                // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+                'error' => PDO::ERRMODE_EXCEPTION,
+             
+            
+                // [optional] Medoo will execute those commands after connected to the database.
+                'command' => [
+                    'SET SQL_MODE=ANSI_QUOTES'
+                ]
+            ]);
+        } catch (Exception $e) {
             echo 'Connection Failed: ' . $e->getMessage();
         }
     }
@@ -20,7 +51,9 @@ class MysqlBaseModel extends BaseModel
     # Create (Insert)
     public function create(array $data): int
     {
-        return 1;
+        $this->connection->insert($this->table, $data);
+        
+        return $this->connection->id();
     }
 
     # Read (select) single | multiple
