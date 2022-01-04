f4b67f53a568befac27fe95233eb3ec590ae7011
seymi <seyedali1377mir@gmail.com>
2022-01-04 16:42:45 +0330

    JsonBaseModel almost implemented

--- App/Models/Product.php
+++ App/Models/Product.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use App\Models\Contracts\JsonBaseModel;
 
-class User extends JsonBaseModel
+class Product extends JsonBaseModel
 {
-    protected $table = 'users';
+    protected $table = 'products';
 }
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 # front controller
 
-use App\Core\Routing\Route;
+use App\Models\Product;
 use App\Core\Routing\Router;
 
 include "bootstrap/init.php";
@@ -10,16 +10,6 @@ include "bootstrap/init.php";
 $router = new Router();
 $router->run();
 
-// $route = '/post/{slug}';
+$productModel = new Product();
 
-// $pattern = "/^".str_replace(['/', '{', '}'], ['\/', '(?<', '>[-%\w]+)'], $route)."$/";
-
-// var_print($route, $pattern, '/^\/post\/(?<slug>[-%\w]+)$/');
-
-// $uri1 = '/post/what-is-php';
-// $uri2 = '/post/14';
-// $uri3 = '/product/12';
-
-// $result = preg_match($pattern, $uri1, $matches);
-
-// var_print($matches);
+var_print($productModel->getAll());
